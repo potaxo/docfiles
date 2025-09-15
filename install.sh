f79b142a496f6a5439f30a0ecc2871a499145644
potaxo <1766811850@qq.com>
2025-09-15 18:13:12 +0900

    add basic conf

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# install.sh
+
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+
+# Variables
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+# List of files/folders to symlink in home directory
+files=".bashrc .vimrc .zshrc .tmux.conf"
+
+# Create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# Change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# Move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
+done
+
+echo "Installation complete!"
